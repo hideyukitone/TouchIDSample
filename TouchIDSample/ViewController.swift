f7350eb50bc56cbb572b69fe13fd450a549dad23
hideyuki_okuni <okuni_hideyuki@matchingagent.co.jp>
2017-11-17 18:42:01 +0900

    Initial Commit

---
+++ TouchIDSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TouchIDSample
+//
+//  Created by 大國 嗣元 on 2017/11/17.
+//  Copyright © 2017年 大國 嗣元. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
